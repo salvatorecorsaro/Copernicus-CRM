a24d55dccdd77a8188f1074367bb6c6a9f4faed5
Salvatore Corsaro <55556636+salvatorecorsaro@users.noreply.github.com>
2021-02-26 12:11:55 +0100

    Add files via upload

---
+++ only-git.sh
@@ -0,0 +1,29 @@
+echo "Setting up project"
+
+git clone https://github.com/C-Inherited/copernicus-microservices-configuration.git copernicus/microservices-directions
+
+git clone https://github.com/C-Inherited/copernicus-eureka-service.git copernicus/eureka-service
+
+git clone https://github.com/C-Inherited/copernicus-config-service.git copernicus/config-service
+
+git clone https://github.com/C-Inherited/copernicus-account-service.git copernicus/account-service
+
+git clone https://github.com/C-Inherited/copernicus-result-service.git copernicus/result-service
+
+git clone https://github.com/C-Inherited/copernicus-validation-service.git copernicus/validation-service
+
+git clone https://github.com/C-Inherited/copernicus-contact-service.git copernicus/contact-service
+
+git clone https://github.com/C-Inherited/copernicus-stats-service.git copernicus/stats-service
+
+git clone https://github.com/C-Inherited/copernicus-opportunities-service.git copernicus/opportunities-service
+
+git clone https://github.com/C-Inherited/copernicus-salesrep-service.git copernicus/salesrep-service
+
+git clone https://github.com/C-Inherited/copernicus-leads-service.git copernicus/leads-service
+
+git clone https://github.com/C-Inherited/copernicus-gateway-service.git copernicus/gateway-service
+
+git clone https://github.com/C-Inherited/Copernicus-CRM.git copernicus/manual 
+
+echo "Copernicus downloaded"
